969c8d8c9e6142c386fd2ce415bd0f519e37af85
GitHub Action <action@github.com>
2022-07-09 18:38:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/smaliav/xmltreeplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/smaliav/xmltreeplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.smaliav.xmltreeplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.smaliav.xmltreeplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/smaliav/xmltreeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/smaliav/xmltreeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.smaliav.xmltreeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smaliav.xmltreeplugin.MyBundle
 
 class MyApplicationService {
 
